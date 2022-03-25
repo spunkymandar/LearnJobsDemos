5ffea30d2447f772a1c5950ac83e05f7b0025cf8
spunkymandar <59153449+spunkymandar@users.noreply.github.com>
2022-03-25 14:00:23 +0530

    Add files via upload

---
+++ LearnJobs_Demos/FirstWebApp/src/main/java/MyServlet.java
@@ -0,0 +1,43 @@
+
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MyServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.getWriter().append("Welcome from Servlet!!");
+		response.getWriter().append("It is :"+LocalTime.now().toString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ LearnJobs_Demos/ThirdWebApp/src/main/java/LoginServlet.java
@@ -0,0 +1,49 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	//MVC pattern 1
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String userid=request.getParameter("txtuser");
+		String userpass=request.getParameter("txtpass");
+		if(userid.equals("mandar") && userpass.equals("mandar123"))
+			response.getWriter().append("Welcome ,"+userid);
+		else
+		{
+			
+			response.getWriter().append("Invalid credentials!!");
+			response.getWriter().append("<a href='login.html'>re-login</a>");
+		
+		}
+	}
+
+}
